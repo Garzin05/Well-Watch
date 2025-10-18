a0532753e92bc9db741ebb6da65f7739d067291b
Marcelo Garzin Sobrinho <contato.luminuz@gmail.com>
2025-10-18 14:52:39 -0300

    chore: remove redundant app_logo and rename main barrel to main_screens

--- Código-Well-Watch/lib/router.dart
+++ Código-Well-Watch/lib/router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:projetowell/screens/auth.dart' as auth_screens;
-import 'package:projetowell/screens/main.dart' as main_screens;
+import 'package:projetowell/screens/main_screens.dart' as main_screens;
 
 class AppRoutes {
   static const login = '/login';
@@ -14,8 +14,10 @@ Map<String, WidgetBuilder> appRoutes() {
   return {
     AppRoutes.login: (context) => const auth_screens.LoginScreen(),
     AppRoutes.welcome: (context) => const auth_screens.WelcomeScreen(),
-    AppRoutes.patientRegistration: (context) => const auth_screens.PatientRegistrationScreen(),
-    AppRoutes.doctorRegistration: (context) => const auth_screens.DoctorRegistrationScreen(),
+    AppRoutes.patientRegistration: (context) =>
+        const auth_screens.PatientRegistrationScreen(),
+    AppRoutes.doctorRegistration: (context) =>
+        const auth_screens.DoctorRegistrationScreen(),
     AppRoutes.home: (context) => const main_screens.HomePage(),
   };
 }
